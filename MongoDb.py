ee0cacc10d16389882fdc42f00bdd62a9dc9ea9e
Vignesh.K <vigneshk24082000@gmail.com>
2023-03-13 10:19:13 +0530

    PythonDB

--- MongoDb.py
+++ MongoDb.py
@@ -4,8 +4,8 @@ mydb=myclient["pythondb"]
 mycol=mydb["users"]
 mydict=[
     {"name":"vignesh","age":22},
-    {"name":"vignesh","age":22},
-    {"name":"vignesh","age":22},
-    {"name":"vignesh","age":22}
+    {"name":"ajith","age":52},
+    {"name":"suresh","age":18},
+    {"name":"balaji","age":24}
 ]
 mycol.insert_many(mydict)
